601f0c57aace03d62e3af917a195559c1794602f
Ankoor Patel <ankoor.p@gmail.com>
2013-10-03 14:19:05 +0100

    label pending tests

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -6,24 +6,31 @@ describe Rover do
   end
 
   it "accepts an array of instructions" do
+    pending
   end
 
   it "engages the engine to move" do
+    pending
   end
 
   it "steers left and right" do
+    pending
   end
 
   it "records its direction while steering" do
+    pending
   end
 
   it "records its position while moving" do
+    pending
   end
 
   it "checks if movements are valid" do
+    pending
   end
 
   it "prints a useful status report" do
+    pending
   end
 
 
