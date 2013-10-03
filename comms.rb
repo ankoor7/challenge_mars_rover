1c69f4865aaf7baa0b7d5990a3dcbdb9fe81a1b6
Ankoor Patel <ankoor.p@gmail.com>
2013-10-03 23:11:49 +0100

    check instruction set for invalid commands

--- comms.rb
+++ comms.rb
@@ -1,14 +1,30 @@
 # This class has functions for communication between "NASA" and any controllers
 
 
+class CommsError < StandardError
+end
+
 class Comms
 # Parse instruction file and return an array of single instruction blocks
   def parse(file)
-    instruction_set = IO.readlines(file).map(&:chomp)
+    @instruction_set = IO.readlines(file).map(&:chomp)
+    check_rover_instruction_set
+    return @instruction_set
   end
 
   # function to send status reports and output to the NASA. (Or the screen in this case)
   def send
   end
 
+  def check_rover_instruction_set
+    @instruction_set.each do |line|
+      line.split('').each do |element|
+        begin
+        raise CommsError, "invalid command in instruction set" unless element =~ /[MLRNSEW\s\d]/
+        rescue => e
+          p e.message
+        end
+      end
+    end
+  end
 end
